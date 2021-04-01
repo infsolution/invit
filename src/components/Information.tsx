bf6a0eeaf365287aa78036caccc313ae7f17dbcf
infsolution <clinfsolution@gmail.com>
2021-04-01 08:37:43 -0300

    add section information

--- src/components/Information.tsx
+++ src/components/Information.tsx
@@ -2,6 +2,21 @@ import styles from '../styles/components/Information.module.css'
 
 export function Information(){
     return(
-        <div>Informações da festa</div>
+        <div className={styles.containerInformation}>
+            <section>
+                <div>
+                    <div>
+                    <strong>Dia da festa</strong>
+                    <p>Sábado</p>
+                    <p>31 de julho 2021 às 20:00</p>
+                    </div>
+                </div>
+                <div>
+                    <div>
+                        <strong>Local</strong>
+                    </div>
+                </div>
+            </section>
+        </div>
     )
 }
