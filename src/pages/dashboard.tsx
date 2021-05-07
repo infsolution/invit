e9ebc8547b75340f219ea682f98284335e37341c
infsolution <clinfsolution@gmail.com>
2021-05-07 13:00:55 -0300

    update

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -9,11 +9,17 @@ import Link from 'next/link'
 import { Inviteds } from '../components/Inviteds'
 import { InferGetStaticPropsType } from 'next'
 import varibles from '../utils/variables'
-import  withAuth from '../utils/withAuth' 
+import  withAuth from '../utils/withAuth'
 import Cookie from 'js-cookie'
 function Dashboard({parties}:InferGetStaticPropsType<typeof getServerSideProps>){
     const router = useRouter()
-    
+    if(!parties){
+        return(
+            <p>
+                pagina zerada
+            </p>
+        )
+    }
     //const { partyhost } = router.query
   return (
     <div className={styles.container} >
@@ -37,7 +43,9 @@ export default withAuth(Dashboard)
 
 
 export async function getServerSideProps(){
+
     const token = Cookie.get('token')
+    let parties = null
     const res = await fetch(`${varibles.urls.url}client/party`,{
         headers: {
             'Content-Type': 'application/json',
@@ -45,15 +53,18 @@ export async function getServerSideProps(){
         },
         method: 'GET'
     })
-    const parties = await res.json()
-    if (parties.length == 0) {
-        return {
-          notFound: true,
-        }
-      }
+    console.log(await res.json())
+    if(res.status >= 400){
+        Cookie.remove('token')
+        parties = null
+    }else{
+        parties = await res.json()
+    }
+
+
     return{
         props:{
-            parties:parties.parties
+            parties:parties
         }
     }
 }
